9499014756bb5a94ec3258d5befa2f21d4e3db1c
taiyuuki <taiyuuki@qq.com>
2025-09-26 10:43:24 +0800

    refactor: improve input argument handling for EPUB files

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ fn main() {
         .version("0.1.0")
         .author("Taiyuuki<taiyuuki@qq.com>")
         .about("Extract images from ePub file and rename them in order.")
-        .arg(Arg::new("input").value_name("EPUB File").required(true))
+        .arg(
+            Arg::new("input")
+                .value_name("EPUB File")
+                .required(true)
+                .num_args(1..)
+                .help("One or more EPUB files to extract images from."),
+        )
         .arg(
             Arg::new("ignore")
                 .short('i')
@@ -19,45 +25,45 @@ fn main() {
                 .help("Ignore images smaller than the specified size in KB."),
         )
         .get_matches();
-
-    if let Some(input) = matches.get_one::<String>("input") {
-        // Get input epub path
-        let input_epub = cwd.as_path().join(input);
-        // Extract images
-        let extractor = extractor::EpubExtractor::new(input_epub.to_str().unwrap());
-        if extractor.is_err() {
-            eprintln!("Not exist or not a valid epub file: {}", input);
-            return;
-        }
-        let mut extractor = extractor.unwrap();
-        println!("Extracting...");
-        const BAR_CHARS: &str = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠇";
+    if let Some(inputs) = matches.get_many::<String>("input") {
         let ignore_size = matches.get_one::<String>("ignore");
-        match ignore_size {
-            Some(size) => {
-                match size.parse::<usize>() {
-                    Ok(size) => {
-                        extractor.set_ignore_size(size);
-                    }
-                    Err(_) => {
-                        eprintln!("Invalid ignore size: {}", size);
-                        return;
-                    }
-                };
+        for input in inputs {
+            let input_epub = cwd.as_path().join(input);
+            println!("\nProcessing: {}", input_epub.display());
+            let extractor = extractor::EpubExtractor::new(input_epub.to_str().unwrap());
+            if extractor.is_err() {
+                eprintln!("Not exist or not a valid epub file: {}", input);
+                continue;
             }
-            None => print!(""),
+            let mut extractor = extractor.unwrap();
+            const BAR_CHARS: &str = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠇";
+            match ignore_size {
+                Some(size) => {
+                    match size.parse::<usize>() {
+                        Ok(size) => {
+                            extractor.set_ignore_size(size);
+                        }
+                        Err(_) => {
+                            eprintln!("Invalid ignore size: {}", size);
+                            continue;
+                        }
+                    };
+                }
+                None => {}
+            }
+            println!("Extracting...");
+            extractor
+                .extract(|s| {
+                    // Print progress
+                    print!(
+                        "\r {} {}% \u{1b}[42m{}\u{1b}[0m",
+                        BAR_CHARS.chars().nth(s % 10).unwrap(),
+                        s,
+                        " ".repeat(s / 4),
+                    );
+                })
+                .unwrap();
+            print!("\r Done \u{1b}[42m{}\u{1b}[0m\n", " ".repeat(20));
         }
-        extractor
-            .extract(|s| {
-                // Print progress
-                print!(
-                    "\r {} {}% \u{1b}[42m{}\u{1b}[0m",
-                    BAR_CHARS.chars().nth(s % 10).unwrap(),
-                    s,
-                    " ".repeat(s / 4),
-                );
-            })
-            .unwrap();
-        print!("\r Done \u{1b}[42m{}\u{1b}[0m", " ".repeat(20));
     }
 }
